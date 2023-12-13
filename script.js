8657d7f0791ab49c2dffd318dd658079d864e33d
Ankita-shrivastav <93003257+Ankita-shrivastav@users.noreply.github.com>
2023-12-13 12:40:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+for(let i=1;i<=20;i++){
+	if(i%3==0){
+		("Fizz");
+	}
+	else if(i%5==0){
+		("Buzz");
+	}
+	else if(i%3==0 && i%5==0){
+		("Fizz Buzz");
+	}
+	else{
+		(i);
+	}
+}
